4edf771caf1b50e49fc5213e3b90b417b124960e
kisenochki <kisenochki@ukr.net>
2018-01-05 07:41:03 +0200

    list

--- src/components/CurrencyList/CurrencyList.vue
+++ src/components/CurrencyList/CurrencyList.vue
@@ -1,15 +1,26 @@
 <script>
   export default {
+    data () {
+      return {}
+    },
     created () {
       this.$store.dispatch('getCurrencyList')
+    },
+    computed: {
+      currencyList: function () {
+        return this.$store.state.currencyList.list
+      }
     }
+
   }
 </script>
 
 <template>
   <v-layout>
-    List
+    <h3>List</h3>
+    <ul>
+      <li v-for="currency in currencyList">{{currency.name}}</li>
+    </ul>
   </v-layout>
 </template>
-
 
