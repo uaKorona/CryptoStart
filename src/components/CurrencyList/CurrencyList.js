0dffd3822668ff624a76f62e2a0f73d8af3faa1a
roman kovalskiy <roman.kovalskyi@trinetix.com>
2018-01-11 13:08:23 +0200

    fix

--- src/components/CurrencyList/CurrencyList.js
+++ src/components/CurrencyList/CurrencyList.js
@@ -2,14 +2,23 @@ export default {
   name: 'CurrencyList',
   data () {
     return {
-      loading: false,
+      loading: true,
       headers: [
-        {text: 'Name', align: 'left', value: 'name'},
-        {text: 'symbol', value: 'symbol'},
-        {text: 'price_usd', value: 'price_usd'},
-        {text: 'Price Graph (7d)', value: 'imageSrc'}
+        {text: '#', value: 'rank', align: 'left'},
+        {text: 'Name', value: 'name', align: 'left'},
+        {text: 'symbol', value: 'symbol', align: 'left'},
+        {text: 'Price', value: 'price_usd'},
+        {text: 'Market Cap', value: 'market_cap_usd'},
+        {text: 'Price (7d)', value: 'imageSrc'}
       ],
-      pagination: {sortBy: 'name'},
+      pagination: {
+        sortBy: 'rank',
+        rowsPerPage: 50
+      },
+      paginationFilter: [
+        20, 50, 100,
+        {text: 'All', value: -1}
+      ],
       search: ''
     }
   },
@@ -19,12 +28,49 @@ export default {
     this.$store
       .dispatch('getCurrencyList')
       .finally(() => {
-        this.loading = false
+        setTimeout(()=> {
+          this.loading = false
+        }, 3000)
       })
   },
   computed: {
     currencyList: function () {
       return this.$store.state.currencyList.list
+    }
+  },
+  filters: {
+    currency(value) {
+      if (!value) return ''
+
+      const maxPartLength = 3
+      const partSeparator = ','
+      const isArrayMaxPart =  index => !((index +1) % maxPartLength)
+
+      let [integerPart, fractionalPart] = value.split('.')
+      let splittedString = integerPart;
+      const isNotLastIntegerSymbol = index => (index + 1) < integerPart.length
+
+      fractionalPart = fractionalPart
+        ? '.' + fractionalPart
+        : ''
+
+      if (integerPart.length > maxPartLength) {
+
+        splittedString = integerPart
+          .split('')
+          .reverse()
+          .map((letter, index) => {
+            if (isArrayMaxPart(index) && isNotLastIntegerSymbol(index )) {
+              return partSeparator + letter
+            }
+            return letter
+          })
+          .reverse()
+          .join('')
+      }
+
+
+      return '$' + splittedString + fractionalPart;
     }
   }
 
